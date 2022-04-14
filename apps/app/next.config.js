e04f47165f14f65f77b85ab809e30f873ced5082
ghcoldsnap <0xa6ebece9938c3e1757be3024d2296666d6f8fc49@mail3.me>
2022-04-14 17:37:20 +0800

    chore(app): use next-compose-plugins

--- apps/app/next.config.js
+++ apps/app/next.config.js
@@ -1,32 +1,31 @@
 const withPWA = require('next-pwa')
+const withPlugins = require('next-compose-plugins')
 const withTM = require('next-transpile-modules')(['ui'])
 const runtimeCaching = require('next-pwa/cache')
 const { i18n } = require('./next-i18next.config')
 
 /** @type {import('next').NextConfig} */
-module.exports = withTM(
-  withPWA({
-    pwa: {
-      dest: 'public',
-      runtimeCaching,
-    },
-    i18n,
-    reactStrictMode: true,
-    async redirects() {
-      return [
-        {
-          source: '/',
-          destination: '/inbox',
-          has: [
-            {
-              type: 'cookie',
-              key: 'authorized',
-              value: 'true',
-            },
-          ],
-          permanent: true,
-        },
-      ]
-    },
-  })
-)
+module.exports = withPlugins([withTM, withPWA], {
+  pwa: {
+    dest: 'public',
+    runtimeCaching,
+  },
+  i18n,
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/inbox',
+        has: [
+          {
+            type: 'cookie',
+            key: 'authorized',
+            value: 'true',
+          },
+        ],
+        permanent: true,
+      },
+    ]
+  },
+})
