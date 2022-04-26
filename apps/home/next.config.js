5a5e63086e71e89df794d964e269d2318feff260
Codemasker <vibes-ins.eth@mail3.me>
2022-04-26 11:47:15 +0800

    feat: add while list page

--- apps/home/next.config.js
+++ apps/home/next.config.js
@@ -5,4 +5,12 @@ const { i18n } = require('./next-i18next.config')
 module.exports = withTM({
   i18n,
   reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
 })
