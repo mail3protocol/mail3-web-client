71e1bf937aab6b6852223d3b25252b0f3cd163d6
ghcoldsnap <0xa6ebece9938c3e1757be3024d2296666d6f8fc49@mail3.me>
2022-05-02 17:07:20 +0800

    feat: add settings page

---
+++ apps/app/components/Tabs/index.tsx
@@ -0,0 +1,51 @@
+import {
+  Center,
+  HStack,
+  StackProps,
+  Text,
+  Box,
+  CenterProps,
+} from '@chakra-ui/react'
+import styled from '@emotion/styled'
+import React from 'react'
+
+export const TabsContainer = styled(HStack)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  border-bottom: 1px solid #f3f3f3;
+
+  @media (max-width: 600px) {
+    border-bottom: none;
+  }
+`
+
+export const Tabs: React.FC<StackProps> = ({ children, ...props }) => (
+  <TabsContainer spacing="10%" {...props}>
+    {children}
+  </TabsContainer>
+)
+
+export interface TabProps extends CenterProps {
+  isActive: boolean
+}
+
+export const Tab: React.FC<TabProps> = ({ isActive, children, ...props }) => (
+  <Center
+    flexDirection="column"
+    cursor="pointer"
+    fontWeight={isActive ? 700 : undefined}
+    fontSize={['14px', '14px', '20px']}
+    {...props}
+  >
+    <Text mb="10px">{children}</Text>
+    <Box
+      w="46px"
+      h="4px"
+      visibility={isActive ? undefined : 'hidden'}
+      borderRadius="32px"
+      bg="black"
+    />
+  </Center>
+)
--- apps/app/pages/settings/signature.tsx
+++ apps/app/pages/settings/signature.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import type { NextPage, GetServerSideProps } from 'next'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { PageContainer } from 'ui'
+import { useTranslation } from 'next-i18next'
+import Link from 'next/link'
+import { Center, Heading } from '@chakra-ui/react'
+import { Navbar } from '../../components/Navbar'
+import { RoutePath } from '../../route/path'
+import { SettingContainer } from '../../components/Settings/SettingContainer'
+import { Tabs, Tab } from '../../components/Tabs'
+import { SettingSignature } from '../../components/Settings/SettingSignature'
 
 export const getServerSideProps: GetServerSideProps = async ({ locale }) => ({
   props: {
@@ -8,6 +17,38 @@ export const getServerSideProps: GetServerSideProps = async ({ locale }) => ({
   },
 })
 
-const SettingsSignature: NextPage = () => <div>Settings</div>
+const SettingsAddressPage: NextPage = () => {
+  const [t] = useTranslation('settings')
+  return (
+    <PageContainer>
+      <Navbar />
+      <SettingContainer>
+        <Center
+          position="relative"
+          w="100%"
+          mb="20px"
+          mt={['20px', '20px', '40px']}
+        >
+          <Heading fontSize={['20px', '20px', '28px']}>
+            {t('settings.title')}
+          </Heading>
+        </Center>
+        <Tabs mb="32px">
+          <Link href={RoutePath.Settings} passHref>
+            <Tab as="a" isActive={false}>
+              {t('settings.tabs.address')}
+            </Tab>
+          </Link>
+          <Link href={RoutePath.SettingSignature} passHref>
+            <Tab as="a" isActive>
+              {t('settings.tabs.signature')}
+            </Tab>
+          </Link>
+        </Tabs>
+        <SettingSignature />
+      </SettingContainer>
+    </PageContainer>
+  )
+}
 
-export default SettingsSignature
+export default SettingsAddressPage
