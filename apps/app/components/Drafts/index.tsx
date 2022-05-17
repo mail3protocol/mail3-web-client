6028c4cd2e3d833edb73aa0271d3194e697a005d
shadow2kk <horong.eth@mail3.me>
2022-05-17 21:59:50 +0800

    feat: add drafts delete

--- apps/app/components/Drafts/index.tsx
+++ apps/app/components/Drafts/index.tsx
@@ -1,44 +1,81 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
 import { useTranslation } from 'next-i18next'
-import React, { useEffect, useState } from 'react'
-import { Box, Center, Flex, Spacer, Wrap, WrapItem } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { Box, Flex, Wrap, WrapItem } from '@chakra-ui/react'
 import { atom, useAtom } from 'jotai'
 import { useDidMount } from 'hooks'
-import { Button } from 'ui'
-import { BoxList } from '../BoxList'
-import SVGDrafts from '../../assets/Drafts.svg'
-import SVGIconEmpty from '../../assets/icon-empty.svg'
-
-const mockList = {
-  message: [
-    {
-      avatar: '',
-      id: 123,
-      emailId: 123,
-      messageId: 123,
-      unseen: true,
-      date: '2022-02-01 / 12:01 am',
-      subject: 'subject subject subject',
-      desc: 'The HEY Team It’s like Mission Control for a contact. See all emails, set up delivery, toggle notifications. The HEY Team It’s like Mission Control for a contact. See all emails, set up delivery, toggle notifications.',
-    },
-  ],
-}
-
-let data: any = mockList.message
-data = [...data, ...data]
-data = [...data, ...data]
-data = [...data, ...data]
+import update from 'immutability-helper'
+import { BoxList, isChooseModeAtom } from '../BoxList'
+import SVGDrafts from '../../assets/drafts.svg'
+import { useAPI } from '../../hooks/useAPI'
+import { useInfiniteScroll } from '../../hooks/useInfiniteScroll'
+import { StickyButtonBox, SuspendButtonType } from '../SuspendButton'
 
 export const DraftsComponent: React.FC = () => {
   const [t] = useTranslation('inbox')
-  const [messages, setMessages] = useState([])
+  const [messages, setMessages] = useState<any>([])
+  const [pageIndex, setPageIndex] = useState(0)
+  const [hasNext, setHasNext] = useState(true)
+  const [isChooseMode, setIsChooseMode] = useAtom(isChooseModeAtom)
+
+  const api = useAPI()
+  const [, setIsFetching] = useInfiniteScroll(fetchDate)
 
   useDidMount(() => {
-    console.log('DraftsComponent useDidMount')
-    setMessages(data)
+    fetchDate(0)
   })
+
+  async function fetchDate(page: number | undefined) {
+    if (!hasNext) return
+
+    const _pageIndex = page === undefined ? pageIndex + 1 : 0
+    setIsFetching(true)
+    const { data } = await api.getMailboxesMessages('INBOX', _pageIndex)
+
+    if (data?.messages?.length) {
+      const newDate: any = update(messages, {
+        $push: data.messages,
+      })
+      setMessages(newDate)
+      setIsFetching(false)
+      setPageIndex(_pageIndex)
+    } else {
+      setHasNext(false)
+    }
+  }
+
+  const updateItem = (index: number) => {
+    console.log('update item index', index)
+
+    const newDate: any = update(messages, {
+      [index]: {
+        isChoose: {
+          $set: !messages[index].isChoose,
+        },
+      },
+    })
+
+    if (newDate.every((item: { isChoose: boolean }) => !item.isChoose)) {
+      setIsChooseMode(false)
+    }
+
+    setMessages(newDate)
+  }
 
   return (
     <>
+      {isChooseMode && (
+        <StickyButtonBox
+          list={[
+            {
+              type: SuspendButtonType.Delete,
+              onClick: () => {
+                console.log('del')
+              },
+            },
+          ]}
+        />
+      )}
       <Flex alignItems="center">
         <Wrap>
           <WrapItem alignItems="center">
@@ -51,15 +88,9 @@ export const DraftsComponent: React.FC = () => {
             fontSize="24px"
             lineHeight="30px"
           >
-            Trash
+            Drafts
           </WrapItem>
         </Wrap>
-
-        <Spacer />
-        <Button>
-          <SVGIconEmpty />
-          <Box marginLeft="10px">Delete All</Box>
-        </Button>
       </Flex>
       <Box
         margin="25px auto"
@@ -69,7 +100,7 @@ export const DraftsComponent: React.FC = () => {
       >
         <Box>
           <Box padding="20px 64px">
-            <BoxList data={messages} />
+            <BoxList data={messages} update={updateItem} />
           </Box>
         </Box>
       </Box>
