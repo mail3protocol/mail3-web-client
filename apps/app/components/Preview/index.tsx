1d70ec667e29aa4c61e9814052f24a3e2c408b7b
shadow2kk <horong.eth@mail3.me>
2022-05-18 16:26:45 +0800

    feat: add avatar preview data

--- apps/app/components/Preview/index.tsx
+++ apps/app/components/Preview/index.tsx
@@ -84,15 +84,11 @@ export const PreviewComponent: React.FC = () => {
       />
       <Center>
         <Box bg="#F3F3F3" padding="4px" borderRadius="47px">
-          <AvatarGroup size="md" max={2}>
-            <Avatar name="Ryan Florence" src="https://bit.ly/ryan-florence" />
-            <Avatar name="Segun Adebayo" src="https://bit.ly/sage-adebayo" />
-            <Avatar name="Kent Dodds" src="https://bit.ly/kent-c-dodds" />
-            <Avatar
-              name="Prosper Otemuyiwa"
-              src="https://bit.ly/prosper-baba"
-            />
-            <Avatar name="Christian Nwamba" src="https://bit.ly/code-beast" />
+          <AvatarGroup size="md" max={10}>
+            {detail?.from && <Avatar name={detail?.from.name} />}
+            {detail?.to.map((item) => (
+              <Avatar name={item.name} key={item.address} />
+            ))}
           </AvatarGroup>
         </Box>
       </Center>
