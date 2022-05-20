463e394e2aebce3f1ca8ee89722a9fc92629499e
Codemasker <vibes-ins.eth@mail3.me>
2022-05-20 16:11:44 +0800

    feat: modify home page rainbow text animation

--- apps/home/components/Home/dao.tsx
+++ apps/home/components/Home/dao.tsx
@@ -8,130 +8,209 @@ import {
   Text,
   VStack,
   Flex,
+  Box,
 } from '@chakra-ui/react'
 import { CONTAINER_MAX_WIDTH } from 'ui'
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import styled from '@emotion/styled'
+import { fromEvent } from 'rxjs'
 import Illustration2Svg from '../../assets/svg/illustration/2.svg'
 import TwitterIconSvg from '../../assets/svg/socialMedia/twitter.svg'
 import MediumIconSvg from '../../assets/svg/socialMedia/medium.svg'
 import MirrorIconSvg from '../../assets/svg/socialMedia/mirror.svg'
 import DiscordIconSvg from '../../assets/svg/socialMedia/discord.svg'
 
-export const Dao = () => (
-  <Center
-    w="full"
-    h="auto"
-    minH={{ base: '667px', md: 'unset' }}
-    bg="#000"
-    color="#fff"
-    px={{ base: '20px', md: '30px' }}
-  >
-    <Grid
+const IllustrationText = styled(Box)`
+  font-family: NanumPenScript-Regular, serif;
+  color: transparent;
+  font-size: 32px;
+  height: auto;
+  padding: 30px 20px 0;
+  -webkit-text-fill-color: transparent;
+  background: linear-gradient(
+    87.97deg,
+    #ffa51e -0.16%,
+    #ddcae2 20.98%,
+    #a9f0ff 43.13%,
+    #fff84f 58.53%,
+    #e6ffb2 76.82%,
+    #3b2aff 92.26%,
+    #ffa51e 100%
+  );
+  -webkit-background-clip: text;
+  animation: run-text-bg 5s linear infinite;
+  transform-origin: top center;
+  @keyframes run-text-bg {
+    from {
+      backgroud-position: 0 0;
+    }
+    to {
+      background-position: 2000px 0;
+    }
+  }
+`
+
+export const Dao = () => {
+  const [illustrationTextScale, setIllustrationTextScale] = useState(1)
+  const illustrationContainerRef = useRef<HTMLDivElement>(null)
+  useEffect(() => {
+    const onChangeIllustrationTextScale = () => {
+      console.log(illustrationContainerRef.current?.offsetWidth)
+      const v = (illustrationContainerRef.current?.offsetWidth || 488) / 488
+      setIllustrationTextScale(v)
+    }
+    const subscriber = fromEvent(window, 'resize').subscribe(
+      onChangeIllustrationTextScale
+    )
+    return () => {
+      subscriber.unsubscribe()
+    }
+  }, [])
+  return (
+    <Center
       w="full"
-      maxW={`${CONTAINER_MAX_WIDTH}px`}
-      h="full"
-      templateColumns={{
-        base: '100%',
-        md: '60% 40%',
-      }}
-      templateRows={{
-        base: 'auto auto auto',
-        md: '60% 40%',
-      }}
-      gap={{ base: '20px', md: 0 }}
-      textAlign={{
-        base: 'center',
-        md: 'left',
-      }}
-      py="64px"
+      h="auto"
+      minH={{ base: '667px', md: 'unset' }}
+      bg="#000"
+      color="#fff"
+      px={{ base: '20px', md: '30px' }}
     >
-      <Flex
-        direction="column"
-        pb={{ base: 0, md: '32px' }}
-        pt={{ base: '20px', md: '26px' }}
-      >
-        <Heading
-          fontSize="56px"
-          lineHeight="56px"
-          mb={{ base: '24px', md: '8px' }}
-          w="full"
-        >
-          Mail3 Postoffice
-        </Heading>
-        <Text
-          fontSize={{ base: '18px', md: '24px' }}
-          lineHeight={{ base: '24px', md: '30px' }}
-          mb="32px"
-          fontWeight="300"
-          w="full"
-        >
-          Mail3 Postoffice is the decentralized autonomous organization owned by
-          the community to govern the protocol development, the collaboration
-          between interested parties, and so on.
-        </Text>
-      </Flex>
-      <Center
-        gridRowEnd={{
-          base: 'unset',
-          md: 'span 2',
+      <Grid
+        w="full"
+        maxW={`${CONTAINER_MAX_WIDTH}px`}
+        h="full"
+        templateColumns={{
+          base: '100%',
+          md: '60% 40%',
         }}
-        gridColumn={{
-          base: 'unset',
-          md: '2',
+        templateRows={{
+          base: 'auto auto auto',
+          md: '60% 40%',
         }}
+        gap={{ base: '20px', md: 0 }}
+        textAlign={{
+          base: 'center',
+          md: 'left',
+        }}
+        py="64px"
       >
-        <Icon as={Illustration2Svg} w="full" h="full" m="auto" />
-      </Center>
-      <VStack justify="end">
-        <Heading fontSize="36px" w="full" mb="26px">
-          Join Now
-        </Heading>
-        <Stack
-          direction="row"
-          fontWeight="500"
-          fontSize="20px"
-          w="full"
-          pb="26px"
-          justify={{
-            base: 'space-between',
-            md: 'start',
-          }}
-          spacing={{
-            base: '30px',
-            md: '56px',
-          }}
-          maxW={{ base: '200px', md: 'unset' }}
+        <Flex
+          direction="column"
+          pb={{ base: 0, md: '32px' }}
+          pt={{ base: '20px', md: '26px' }}
         >
-          <Link href="#">
+          <Heading
+            fontSize="56px"
+            lineHeight="56px"
+            mb={{ base: '24px', md: '8px' }}
+            w="full"
+          >
+            Mail3 Postoffice
+          </Heading>
+          <Text
+            fontSize={{ base: '18px', md: '24px' }}
+            lineHeight={{ base: '24px', md: '30px' }}
+            mb="32px"
+            fontWeight="300"
+            w="full"
+          >
+            Mail3 Postoffice is the decentralized autonomous organization owned
+            by the community to govern the protocol development, the
+            collaboration between interested parties, and so on.
+          </Text>
+        </Flex>
+        <Center
+          gridRowEnd={{
+            base: 'unset',
+            md: 'span 2',
+          }}
+          gridColumn={{
+            base: 'unset',
+            md: '2',
+          }}
+          ref={illustrationContainerRef}
+        >
+          <Box
+            w="full"
+            h="full"
+            position="relative"
+            style={{
+              transform: `scale(${illustrationTextScale})`,
+              height: `${402 * illustrationTextScale}px`,
+            }}
+          >
             <Icon
-              as={TwitterIconSvg}
-              w={{ base: '25px', md: '40px' }}
-              h="auto"
+              as={Illustration2Svg}
+              w="488px"
+              h="402px"
+              mt="auto"
+              mr="auto"
+              position="absolute"
+              top="0"
+              right="0"
             />
-          </Link>
-          <Link href="#">
-            <Icon
-              as={MediumIconSvg}
-              w={{ base: '25px', md: '40px' }}
-              h="auto"
-            />
-          </Link>
-          <Link href="#">
-            <Icon
-              as={MirrorIconSvg}
-              w={{ base: '25px', md: '40px' }}
-              h="auto"
-            />
-          </Link>
-          <Link href="#">
-            <Icon
-              as={DiscordIconSvg}
-              w={{ base: '25px', md: '40px' }}
-              h="auto"
-            />
-          </Link>
-        </Stack>
-      </VStack>
-    </Grid>
-  </Center>
-)
+            <IllustrationText
+              w="410px"
+              position="absolute"
+              top="0"
+              right="0"
+              bgClip="text"
+            >
+              Together we protect our own realm, make our own rules.
+            </IllustrationText>
+          </Box>
+        </Center>
+        <VStack justify="end">
+          <Heading fontSize="36px" w="full" mb="26px">
+            Join Now
+          </Heading>
+          <Stack
+            direction="row"
+            fontWeight="500"
+            fontSize="20px"
+            w="full"
+            pb="26px"
+            justify={{
+              base: 'space-between',
+              md: 'start',
+            }}
+            spacing={{
+              base: '30px',
+              md: '56px',
+            }}
+            maxW={{ base: '200px', md: 'unset' }}
+          >
+            <Link href="#">
+              <Icon
+                as={TwitterIconSvg}
+                w={{ base: '25px', md: '40px' }}
+                h="auto"
+              />
+            </Link>
+            <Link href="#">
+              <Icon
+                as={MediumIconSvg}
+                w={{ base: '25px', md: '40px' }}
+                h="auto"
+              />
+            </Link>
+            <Link href="#">
+              <Icon
+                as={MirrorIconSvg}
+                w={{ base: '25px', md: '40px' }}
+                h="auto"
+              />
+            </Link>
+            <Link href="#">
+              <Icon
+                as={DiscordIconSvg}
+                w={{ base: '25px', md: '40px' }}
+                h="auto"
+              />
+            </Link>
+          </Stack>
+        </VStack>
+      </Grid>
+    </Center>
+  )
+}
