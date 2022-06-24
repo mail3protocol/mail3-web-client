a8156501e1f56a4c83a3e45e6e8e0d64aa8dd01b
shadow2kk <horong.eth@mail3.me>
2022-06-24 17:09:17 +0800

    feat: add new share card style

--- packages/ui/src/ProfileCard/index.tsx
+++ packages/ui/src/ProfileCard/index.tsx
@@ -3,55 +3,90 @@ import React, { forwardRef } from 'react'
 import styled from '@emotion/styled'
 import QrCode from 'qrcode.react'
 import { Avatar, Button } from 'ui'
-// @ts-ignore
+import classNames from 'classnames'
+
 import LogoSvg from 'assets/svg/logo-pure.svg'
-// @ts-ignore
-import PngBorder from 'assets/profile/border.png'
 import PngMailMeButton from 'assets/profile/mail-me-button.png'
+import PngBorder from './assets/border.png'
+import PngAddressBorder from './assets/address-border.png'
+import PngSeal from './assets/seal.png'
 
 interface ProfileCardProps {
   mailAddress: string
   isPic?: boolean
   homeUrl: string
   children?: React.ReactNode
+  isDev?: boolean
 }
 
 const Container = styled(Box)`
-  background: #fff;
   height: 566px;
   width: 375px;
   top: -9999px;
   left: -9999px;
-  /* top: 10px; */
-  /* left: 10px; */
   position: absolute;
   border-radius: 20px;
+  background-image: url(${PngBorder.src});
+  background-repeat: no-repeat;
+  background-size: 100%;
 
   &.is-picture {
     top: 0;
     left: 0;
     position: relative;
     pointer-events: none;
-    box-shadow: 1px 1px 10px #ccc;
 
     .border {
-      display: none;
+      filter: drop-shadow(0px 0px 8px rgba(0, 0, 0, 0.15));
     }
+
+    .address .p {
+      // fix html2canvas bug
+      margin-top: 0;
+      padding-bottom: 0;
+    }
+  }
+
+  &.is-dev {
+    top: 10px;
+    left: 10px;
+    position: absolute;
+  }
+
+  .content {
+    z-index: 2;
+    height: 100%;
+    position: relative;
   }
 
   .address {
     max-width: 90%;
-    margin-top: 15px;
-    background: #f3f3f3;
-    border-radius: 16px;
+    /* margin-top: 15px; */
+    /* background: #f3f3f3; */
+    /* border-radius: 16px; */
     padding: 13px;
     text-align: center;
+    position: relative;
 
     .p {
       font-style: normal;
       font-weight: 600;
       font-size: 19px;
       line-height: 1;
+      z-index: 2;
+      position: relative;
+      // fix html2canvas bug
+      margin-top: -10px;
+      padding-bottom: 10px;
+    }
+
+    .address-border {
+      top: 0;
+      left: 0;
+      height: 100%;
+      width: 100%;
+      z-index: 1;
+      position: absolute;
     }
   }
 
@@ -70,10 +105,24 @@ const Container = styled(Box)`
     position: absolute;
   }
 
+  .avatar-wrap {
+    position: relative;
+    padding-bottom: 15px;
+    .seal {
+      top: 0;
+      left: 0;
+      width: 268px;
+      max-width: none;
+      z-index: 0;
+      position: absolute;
+      transform: translate(-57px, -60px);
+    }
+  }
+
   .border {
     width: 100%;
     height: 100%;
-    z-index: 0;
+    z-index: 1;
     top: 0;
     left: 0;
     position: absolute;
@@ -81,37 +130,51 @@ const Container = styled(Box)`
 `
 
 export const ProfileCard = forwardRef<HTMLDivElement, ProfileCardProps>(
-  ({ mailAddress, isPic, children, homeUrl }, ref) => {
+  ({ mailAddress, isPic, isDev, children, homeUrl }, ref) => {
     const address = mailAddress.substring(0, mailAddress.indexOf('@'))
 
     return (
-      <Container ref={ref} className={isPic ? 'is-picture' : ''}>
+      <Container
+        ref={ref}
+        className={classNames({
+          'is-picture': isPic,
+          'is-dev': isDev,
+        })}
+      >
         <Image src={PngBorder.src} className="border" alt="" />
-        <Center flexDirection="column">
-          <Box pt="20px">
-            <LogoSvg />
-          </Box>
-          <Box mt="35px">
-            <Avatar address={address} w="72px" h="72px" />
-          </Box>
-          <Box className="address">
-            <Text className="p">{mailAddress}</Text>
-          </Box>
-          <HStack spacing="24px" mt="16px">
-            {children}
-          </HStack>
+        <Box className="content">
+          <Center flexDirection="column">
+            <Box pt="35px">
+              <LogoSvg />
+            </Box>
+            <Box mt="20px" className="avatar-wrap">
+              <Box position="relative" zIndex={2}>
+                <Avatar address={address} w="72px" h="72px" />
+              </Box>
+              <Image src={PngSeal.src} className="seal" />
+            </Box>
+            <Box className="address">
+              <Text className="p">{mailAddress}</Text>
+              <Box className="address-border">
+                <Image w="100%" h="100%" src={PngAddressBorder.src} alt="" />
+              </Box>
+            </Box>
+            <HStack spacing="24px" mt="16px">
+              {children}
+            </HStack>
 
-          <Box className="qrCode">
-            <QrCode
-              value={`${homeUrl}/${address}?utm_medium=qrcode`}
-              size={130}
-            />
-          </Box>
+            <Box className="qrCode">
+              <QrCode
+                value={`${homeUrl}/${address}?utm_medium=qrcode`}
+                size={130}
+              />
+            </Box>
 
-          <Button className="button">
-            <Image src={PngMailMeButton.src} w="100%" height="100%" />
-          </Button>
-        </Center>
+            <Button className="button">
+              <Image src={PngMailMeButton.src} w="100%" height="100%" />
+            </Button>
+          </Center>
+        </Box>
       </Container>
     )
   }
