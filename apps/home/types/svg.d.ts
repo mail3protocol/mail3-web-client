e8dbaff18ada318b927d4ea75ddc0cede2ccb934
shadow2kk <horong.eth@mail3.me>
2022-06-28 16:45:13 +0800

    refactor: fix eslint

--- apps/home/types/svg.d.ts
+++ apps/home/types/svg.d.ts
@@ -2,13 +2,3 @@ declare module '*.svg?url' {
   const path: string
   export default path
 }
-declare namespace JSX {
-  type Mail3MeProps = React.HTMLAttributes<HTMLElement> & {
-    css: string
-    icon_style: string
-    to: string
-  }
-  interface IntrinsicElements {
-    'mail3-me': React.DetailedHTMLProps<Mail3MeProps, HTMLElement>
-  }
-}
