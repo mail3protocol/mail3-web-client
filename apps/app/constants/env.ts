1848eab5a8a73863ff8fa2d5ff27859e22eb641a
ghcoldsnap <0xa6ebece9938c3e1757be3024d2296666d6f8fc49@mail3.me>
2022-07-10 20:22:17 +0800

    fix: hard code ga id

--- apps/app/constants/env.ts
+++ apps/app/constants/env.ts
@@ -18,7 +18,8 @@ export const WHITE_LIST_DOC_URL =
 
 export const HOME_URL = process.env.NEXT_PUBLIC_HOME_URL || 'https://mail3.me'
 
-export const GOOGLE_ANALYTICS_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID
+export const GOOGLE_ANALYTICS_ID =
+  process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID ?? 'G-WH0BKBPFWP'
 
 export const MORE_DETAILS_LINK =
   process.env.NEXT_PUBLIC_MORE_DETAILS_LINK ||
