5bf6e5f5f603070fce32bb74edb0d36ea5a2ee8f
vibes-ins.eth <107289443+Codemasker@users.noreply.github.com>
2022-08-22 09:56:28 +0800

    feat: add ipfs signing dialog (#121)

--- apps/app/components/IpfsModal/index.tsx
+++ apps/app/components/IpfsModal/index.tsx
@@ -8,8 +8,11 @@ import {
   Text,
   Image,
   Center,
+  Box,
+  useDisclosure,
+  Flex,
 } from '@chakra-ui/react'
-import React, { useCallback, useState } from 'react'
+import React, { useCallback } from 'react'
 import { Button } from 'ui'
 import {
   NotConnectWallet,
@@ -22,10 +25,60 @@ import DesktopIpfsGuidePng from '../../assets/ipfs-guide/desktop.png'
 import MobileIpfsGuidePng from '../../assets/ipfs-guide/mobile.png'
 import { useAPI } from '../../hooks/useAPI'
 import { digestMessage } from '../../utils'
+import GIFLoading from '../../assets/mailbox/loading.gif'
 
 const stringToBeSigned = `Generate MESSAGE ENCRYPTION key for me and I authorize current dApp to access my MESSAGE ENCRYPTION key. (This operation won’t affect your digital assets.)
 
 nonce=1`
+
+const SigningDialog: React.FC<{ isOpen: boolean; onClose: () => void }> = ({
+  isOpen,
+  onClose,
+}) => {
+  const { t } = useTranslation('ipfs_modal')
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      isCentered
+      closeOnEsc={false}
+      closeOnOverlayClick={false}
+    >
+      <ModalOverlay />
+      <ModalContent
+        maxW="641px"
+        rounded={{ base: '24px', md: '48px' }}
+        pb="32px"
+        w="calc(100% - 40px)"
+        h="494px"
+      >
+        <ModalHeader
+          as={Flex}
+          borderBottom="1px solid #F4F4F4"
+          h="95px"
+          textAlign="center"
+          fontSize={{ base: '16px', md: '24px' }}
+          py="0"
+          px="20px"
+          flex={1}
+          maxH="95px"
+        >
+          <Box my="auto" w="full">
+            {t('signing_modal.title')}
+          </Box>
+        </ModalHeader>
+        <ModalBody px="20px" fontSize={{ base: '12px', md: '16px' }}>
+          <Text maxW="495px" mx="auto" whiteSpace="pre-line" textAlign="center">
+            {t('signing_modal.content')}
+          </Text>
+          <Center>
+            <Image src={GIFLoading} alt="loading_gif" h="278px" w="auto" />
+          </Center>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  )
+}
 
 export const IpfsModal: React.FC<{
   isOpen: boolean
@@ -38,10 +91,14 @@ export const IpfsModal: React.FC<{
   const signMessage = useSignMessage()
   useEagerConnect(isForceConnectWallet)
   const { onOpen: onOpenWalletDialog } = useConnectWalletDialog()
-  const [isGenerating, setIsGenerating] = useState(false)
+  const {
+    isOpen: isOpenSigningDialog,
+    onOpen: onOpenSigningDialog,
+    onClose: onCloseSigningDialog,
+  } = useDisclosure()
   const onGenerateKey = useCallback(async () => {
     try {
-      setIsGenerating(true)
+      onOpenSigningDialog()
       const signedData = await signMessage(stringToBeSigned)
       const signedString =
         typeof signedData === 'string' ? signedData : signedData.signature
@@ -55,64 +112,79 @@ export const IpfsModal: React.FC<{
         onOpenWalletDialog()
       }
     } finally {
-      setIsGenerating(false)
+      onCloseSigningDialog()
     }
   }, [onAfterSignature, signMessage])
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose} isCentered>
-      <ModalOverlay />
-      <ModalContent
-        maxW="641px"
-        rounded={{ base: '24px', md: '48px' }}
-        pb="32px"
-        w="calc(100% - 40px)"
+    <>
+      <SigningDialog
+        isOpen={isOpenSigningDialog}
+        onClose={onCloseSigningDialog}
+      />
+      <Modal
+        isOpen={isOpenSigningDialog ? false : isOpen}
+        onClose={onClose}
+        isCentered
       >
-        <ModalHeader
-          borderBottom="1px solid #F4F4F4"
-          lineHeight="95px"
-          textAlign="center"
-          fontSize="24px"
-          p="0"
+        <ModalOverlay />
+        <ModalContent
+          maxW="641px"
+          rounded={{ base: '24px', md: '48px' }}
+          pb="32px"
+          w="calc(100% - 40px)"
         >
-          {t('title')}
-        </ModalHeader>
-        <ModalBody>
-          <Text maxW="495px" mx="auto" whiteSpace="pre-line" textAlign="center">
-            {t('content')}
-          </Text>
-          <Center
-            bg="rgba(244, 251, 241, 0.5)"
-            rounded="24px"
-            px="24px"
-            pt="10px"
-            minH="216px"
-            mt="16px"
+          <ModalHeader
+            borderBottom="1px solid #F4F4F4"
+            lineHeight="95px"
+            textAlign="center"
+            fontSize="24px"
+            p="0"
           >
-            <Image
-              src={DesktopIpfsGuidePng}
-              display={{ base: 'none', md: 'block' }}
-            />
-            <Image
-              src={MobileIpfsGuidePng}
-              display={{ base: 'block', md: 'none' }}
-            />
-          </Center>
-        </ModalBody>
-
-        <ModalFooter display="flex" justifyContent="center" p="0" mt="24px">
-          <Button
-            w="246px"
-            lineHeight="46px"
-            h="46px"
-            onClick={onGenerateKey}
-            isLoading={isGenerating}
-            isDisabled={isGenerating}
-          >
-            {t('generate_key')}
-          </Button>
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
+            {t('title')}
+          </ModalHeader>
+          <ModalBody px="20px" fontSize={{ base: '12px', md: '16px' }}>
+            <Text
+              maxW="495px"
+              mx="auto"
+              whiteSpace="pre-line"
+              textAlign="center"
+            >
+              {t('content')}
+            </Text>
+            <Center
+              bg="rgba(244, 251, 241, 0.5)"
+              rounded="24px"
+              px="24px"
+              pt="10px"
+              minH="216px"
+              mt="16px"
+            >
+              <Image
+                src={DesktopIpfsGuidePng}
+                display={{ base: 'none', md: 'block' }}
+              />
+              <Image
+                src={MobileIpfsGuidePng}
+                display={{ base: 'block', md: 'none' }}
+                w="140px"
+              />
+            </Center>
+          </ModalBody>
+          <ModalFooter display="flex" justifyContent="center" p="0" mt="24px">
+            <Button
+              w="246px"
+              lineHeight="46px"
+              h="46px"
+              onClick={onGenerateKey}
+              isLoading={isOpenSigningDialog}
+              isDisabled={isOpenSigningDialog}
+            >
+              {t('generate_key')}
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
   )
 }
