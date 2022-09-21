9704c63a07d4e6bd76ba78c30fa1eb06a6746f82
ghcoldsnap <0xa6ebece9938c3e1757be3024d2296666d6f8fc49@mail3.me>
2022-09-21 13:02:22 +0800

    chore: debugger zilpay

--- apps/app/hooks/useLogin.ts
+++ apps/app/hooks/useLogin.ts
@@ -299,6 +299,7 @@ export const useWalletChange = () => {
   )
   const handleZilpayAccountChanged = useCallback(
     (acc: any) => {
+      console.log(acc, loginAccount)
       if (loginAccount && !loginAccount.startsWith('zil')) {
         return
       }
