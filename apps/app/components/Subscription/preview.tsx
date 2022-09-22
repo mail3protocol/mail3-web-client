3a56efc588df853adf19cba41cccf0bd01943815
shadow2kk <horong.eth@mail3.me>
2022-09-22 11:49:28 +0800

    feat: add preview ui

--- apps/app/components/Subscription/preview.tsx
+++ apps/app/components/Subscription/preview.tsx
@@ -1,13 +1,15 @@
-import { Box } from '@chakra-ui/react'
+import { Avatar, Box, Flex, Spacer, Text } from '@chakra-ui/react'
 import styled from '@emotion/styled'
 import { atom, useAtomValue } from 'jotai'
 import React, { useEffect } from 'react'
+import { RenderHTML } from '../Preview/parser'
 
 export const SubPreviewIdAtom = atom(null)
 
 const Container = styled(Box)`
   flex: 2;
   height: 100%;
+  padding: 55px;
   overflow: hidden;
   overflow-y: scroll;
 `
@@ -16,6 +18,14 @@ export const SubPreview: React.FC = () => {
   const id = useAtomValue(SubPreviewIdAtom)
 
   // useQuery [id]
+  const mock = {
+    uuid: 'string',
+    subject: 'string',
+    writer_name: 'string',
+    writer_uuid: 'string',
+    content: 'string',
+    created_at: 'string',
+  }
 
   useEffect(() => {
     console.log('preview', id)
@@ -27,5 +37,29 @@ export const SubPreview: React.FC = () => {
 
   // content (parse html)
 
-  return <Container />
+  return (
+    <Container>
+      <Flex w="100%" align="center">
+        <Flex align="center">
+          <Avatar w="43px" h="43px" />
+          <Box ml="15px" fontWeight={600} fontSize="14px">
+            123
+          </Box>
+        </Flex>
+        <Spacer />
+        <Box fontWeight={500} fontSize="14px">
+          Aug 27 / 9:07 am{' '}
+        </Box>
+      </Flex>
+      <Text fontWeight={700} fontSize="28px" textAlign="center">
+        The More Important the Work, the More Important the Rest
+      </Text>
+      <RenderHTML
+        html="content"
+        attachments={[]}
+        messageId=""
+        from={{ name: '', address: '' }}
+      />
+    </Container>
+  )
 }
