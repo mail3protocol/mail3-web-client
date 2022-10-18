199687aac0cdc216d43864aaa1f6309f177c74d8
vibes-ins.eth <vibes-ins.eth@mail3.me>
2022-10-18 15:46:18 +0800

    feat: Supplementary conditions to `isNextDay`

--- packages/shared/src/isNextDay.ts
+++ packages/shared/src/isNextDay.ts
@@ -3,5 +3,6 @@ import dayjs from 'dayjs'
 export function isNextDay(time: string | number | dayjs.Dayjs) {
   const currentTime =
     typeof time === 'string' || typeof time === 'number' ? dayjs(time) : time
-  return currentTime.date() < dayjs().date()
+  const now = dayjs()
+  return currentTime.date() < now.date() && currentTime.isBefore(now)
 }
