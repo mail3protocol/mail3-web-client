a58e22abfc05fd897af83719aa0a548bd1d17e8c
vibes-ins.eth <vibes-ins.eth@mail3.me>
2023-02-09 16:52:51 +0800

    refactor(app): refactor the processes about notification

--- apps/app/hooks/useNotification.ts
+++ apps/app/hooks/useNotification.ts
@@ -4,136 +4,12 @@ import { useQuery } from 'react-query'
 import { useAtom } from 'jotai'
 import { useToast } from 'hooks'
 import { IS_CHROME, IS_FIREFOX, IS_MOBILE } from '../constants/utils'
-import {
-  getIsEnabledNotification,
-  getNotificationPermission,
-  userPropertiesAtom,
-} from './useLogin'
+import { getNotificationPermission, userPropertiesAtom } from './useLogin'
 import { useAPI } from './useAPI'
 import { useDeleteFCMToken, useGetFCMToken } from './useFCMToken'
-import { Query } from '../api/query'
 
 export function useNotification(shouldReload = true) {
-  const api = useAPI()
-  const [userInfo, setUserInfo] = useAtom(userPropertiesAtom)
-  const [permission, setPermission] = useState<
-    NotificationPermission | PermissionState
-  >(getNotificationPermission())
-  const [
-    isSwitchingWebPushNotificationState,
-    setIsSwitchingWebPushNotificationState,
-  ] = useState(false)
-  const onDeleteFCMToken = useDeleteFCMToken()
-  const getFCMToken = useGetFCMToken()
-  const toast = useToast()
-
-  const onLoadMessagingToken = useCallback(
-    async (state: 'enabled' | 'disabled') => {
-      if (state === 'disabled') {
-        await onDeleteFCMToken()
-      } else if (state === 'enabled') {
-        await getFCMToken()
-      }
-    },
-    [getFCMToken, onDeleteFCMToken]
-  )
-
-  const onSwitchWebPushNotificationState = useCallback(
-    async (state: 'enabled' | 'disabled') => {
-      if (isSwitchingWebPushNotificationState) return
-      setIsSwitchingWebPushNotificationState(true)
-      try {
-        await onLoadMessagingToken(state)
-        await api.switchUserWebPushNotification(
-          state === 'enabled' ? 'active' : 'stale'
-        )
-        setUserInfo((info) => ({
-          ...info,
-          notification_state: state,
-        }))
-      } catch (err) {
-        const message = (err as any)?.message as any
-        toast(message, {
-          status: 'error',
-        })
-        console.error(err)
-      } finally {
-        setIsSwitchingWebPushNotificationState(false)
-      }
-    },
-    [
-      api,
-      isSwitchingWebPushNotificationState,
-      setIsSwitchingWebPushNotificationState,
-      onLoadMessagingToken,
-      setUserInfo,
-    ]
-  )
-
-  const { data: apiUserInfo, refetch } = useQuery(
-    [Query.GetUserInfo],
-    async () => api.getUserInfo().then((r) => r.data),
-    {
-      onSuccess(d) {
-        setUserInfo((u) => ({
-          ...u,
-          notification_state: d.web_push_notification_state,
-        }))
-      },
-    }
-  )
-  const webPushNotificationState =
-    apiUserInfo?.web_push_notification_state ||
-    userInfo?.notification_state ||
-    'disabled'
-
-  async function onChangePermission(newPermission: NotificationPermission) {
-    setPermission(newPermission)
-    setUserInfo((info) => ({
-      ...info,
-      ...getIsEnabledNotification(newPermission),
-    }))
-    await onSwitchWebPushNotificationState(
-      newPermission === 'granted' ? 'enabled' : 'disabled'
-    )
-    await refetch()
-  }
-
-  function onSubscribeNavigatorPermissions() {
-    if ('permissions' in navigator) {
-      return defer(() =>
-        from(navigator.permissions.query({ name: 'notifications' }))
-      )
-        .pipe(
-          switchMap((notificationPerm) => fromEvent(notificationPerm, 'change'))
-        )
-        .subscribe(async (event) => {
-          const target = event.target as PermissionStatus
-          const newPermission = target.state as NotificationPermission
-          await onChangePermission(target.state as NotificationPermission)
-          if (newPermission === 'granted' && shouldReload) {
-            location.reload()
-          }
-        })
-    }
-    return null
-  }
-
-  async function onCheckNotificationStatus() {
-    if (permission === 'granted' && webPushNotificationState === 'enabled') {
-      await onSwitchWebPushNotificationState('enabled')
-    }
-  }
-
-  useEffect(() => {
-    onCheckNotificationStatus()
-    const navigatorPermissionsSubscriber = onSubscribeNavigatorPermissions()
-    return () => {
-      navigatorPermissionsSubscriber?.unsubscribe()
-    }
-  }, [])
-
-  const { data: isBrowserSupport, isLoading: isBrowserSupportChecking } =
+  const { data: isBrowserSupport, isLoading: isCheckingBrowserSupport } =
     useQuery(
       ['isSupportedFCM'],
       async () => {
@@ -148,22 +24,128 @@ export function useNotification(shouldReload = true) {
         refetchOnReconnect: false,
       }
     )
+  const [permission, setPermission] = useState<
+    NotificationPermission | PermissionState
+  >(getNotificationPermission())
+  const [
+    isSwitchingWebPushNotificationState,
+    setIsSwitchingWebPushNotificationState,
+  ] = useState(false)
+  const [userInfo, setUserInfo] = useAtom(userPropertiesAtom)
+  const api = useAPI()
+  const webPushNotificationState = userInfo?.notification_state || 'disabled'
+  const deleteFCMToken = useDeleteFCMToken()
+  const getFCMToken = useGetFCMToken()
+  const toast = useToast()
+
+  const onSwitchWebPushNotificationState = useCallback(
+    async (state: 'enabled' | 'disabled') => {
+      if (isSwitchingWebPushNotificationState) return
+      setIsSwitchingWebPushNotificationState(true)
+      try {
+        if (state === 'disabled') {
+          await deleteFCMToken()
+        } else if (state === 'enabled') {
+          await getFCMToken()
+        }
+        await api.switchUserWebPushNotification(
+          state === 'enabled' ? 'active' : 'stale'
+        )
+        setUserInfo((info) => ({
+          ...info,
+          notification_state: state,
+        }))
+        setIsSwitchingWebPushNotificationState(false)
+      } catch (err) {
+        setIsSwitchingWebPushNotificationState(false)
+        console.error(err)
+        const message = (err as any)?.message as any
+        toast(message, {
+          status: 'error',
+        })
+        throw err
+      }
+    },
+    [
+      api,
+      isSwitchingWebPushNotificationState,
+      setIsSwitchingWebPushNotificationState,
+      setUserInfo,
+      deleteFCMToken,
+      getFCMToken,
+    ]
+  )
 
   const openNotification = useCallback(async () => {
     // eslint-disable-next-line compat/compat
-    const newPermission = await window.Notification.requestPermission()
-    if (newPermission !== 'granted') return newPermission
-    if (permission === newPermission) {
-      await onChangePermission(newPermission)
+    if (window.Notification.permission === 'default') {
+      // eslint-disable-next-line compat/compat
+      await window.Notification.requestPermission()
+      return
     }
-    return newPermission
-  }, [permission, onChangePermission])
+    await onSwitchWebPushNotificationState('enabled')
+  }, [onSwitchWebPushNotificationState])
+
+  function onSubscribeNavigatorPermissions() {
+    if ('permissions' in navigator) {
+      return defer(() =>
+        from(navigator.permissions.query({ name: 'notifications' }))
+      )
+        .pipe(
+          switchMap((notificationPerm) => fromEvent(notificationPerm, 'change'))
+        )
+        .subscribe(async (event) => {
+          const target = event.target as PermissionStatus
+          const newPermission = target.state as NotificationPermission
+          if (newPermission === 'granted') {
+            await onSwitchWebPushNotificationState('enabled')
+            if (shouldReload) {
+              location.reload()
+            }
+          }
+          setPermission(newPermission)
+        })
+    }
+    return null
+  }
+
+  async function checkTokenStatus() {
+    const { createStore, get, keys } = await import('idb-keyval')
+    const store = createStore(
+      'firebase-messaging-database',
+      'firebase-messaging-store'
+    )
+    const currentKeys = await keys(store)
+    const token =
+      currentKeys.length > 0
+        ? ((await get(currentKeys[0], store))?.token as string)
+        : undefined
+    if (!token) {
+      await onSwitchWebPushNotificationState('enabled')
+      return
+    }
+    const currentTokenState = await api
+      .getRegistrationTokenState(token)
+      .then((res) => res.data.state)
+      .catch(() => 'stale')
+    if (currentTokenState === 'stale') {
+      await api.updateRegistrationToken(token, 'active')
+    }
+  }
+
+  useEffect(() => {
+    checkTokenStatus()
+    const s = onSubscribeNavigatorPermissions()
+    return () => {
+      s?.unsubscribe()
+    }
+  }, [])
 
   return {
     permission,
-    webPushNotificationState,
     isBrowserSupport,
-    isBrowserSupportChecking,
+    isBrowserSupportChecking: isCheckingBrowserSupport,
     openNotification,
+    webPushNotificationState,
   }
 }
