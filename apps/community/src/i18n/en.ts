50c97103a83a62ac407a8204098d55f2277c1f3f
vibes-ins.eth <vibes-ins.eth@mail3.me>
2023-02-14 16:31:16 +0800

    feat(community): modify the registration link

--- apps/community/src/i18n/en.ts
+++ apps/community/src/i18n/en.ts
@@ -132,11 +132,12 @@ export default {
   hooks: {
     register_dialog: {
       title: 'Apply for your Web3 Subscription',
-      description: `Please click the "mail<sup>3</sup> me" button below to submit your application. It may take some time for your application to receive a response.`,
+      description: `Please click the "Apply" button to submit your application. It may take some time for your application to receive a response.`,
       no_white_list_description:
         'If you would like to apply for access to "mail<sup>3</sup> Community". <br/><br/>Please visit mail<sup>3</sup> official website to claim your Mail3 first.',
       register_mail_default_subject: '[Apply for Mail3 Community whitelist]',
       continue: 'Continue',
+      apply: 'Apply',
     },
   },
   new_message: {
