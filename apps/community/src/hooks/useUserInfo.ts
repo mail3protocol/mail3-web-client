912129e645977ee38871d145a7533dc09e25c551
shadow2kk <horong.eth@mail3.me>
2023-03-15 14:37:59 +0800

    refactor: reduce the number of requests

--- apps/community/src/hooks/useUserInfo.ts
+++ apps/community/src/hooks/useUserInfo.ts
@@ -1,7 +1,9 @@
-import { atom } from 'jotai'
-import { useAtomValue, useUpdateAtom } from 'jotai/utils'
-import { useEffect } from 'react'
+import { useAccount } from 'hooks'
+import { atom, useAtom } from 'jotai'
+import { useUpdateAtom } from 'jotai/utils'
+import { useQuery } from 'react-query'
 import { UserSettingResponse } from '../api/modals/UserInfoResponse'
+import { QueryKey } from '../api/QueryKey'
 import { useAPI } from './useAPI'
 
 interface UserInfo extends UserSettingResponse {}
@@ -11,13 +13,27 @@ const userInfoAtom = atom<UserInfo | null>(null)
 export const useSetUserInfo = () => useUpdateAtom(userInfoAtom)
 
 export const useUserInfo = () => {
-  const info = useAtomValue(userInfoAtom)
-  const setInfo = useSetUserInfo()
+  const [info, setInfo] = useAtom(userInfoAtom)
+  const account = useAccount()
   const api = useAPI()
-  useEffect(() => {
-    if (info) return
-    api.getUserSetting().then(({ data }) => setInfo(data))
-  }, [])
+
+  useQuery(
+    [QueryKey.GetUserSetting, account],
+    async () => {
+      const res = await api.getUserSetting()
+      return res.data
+    },
+    {
+      enabled: !info,
+      refetchIntervalInBackground: false,
+      refetchOnMount: false,
+      refetchOnReconnect: false,
+      refetchOnWindowFocus: false,
+      onSuccess(d) {
+        setInfo(d)
+      },
+    }
+  )
 
   return info
 }
