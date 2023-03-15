9e2ae9f1d2057ed32b980e53cca485e7a10ab5fe
shadow2kk <horong.eth@mail3.me>
2023-03-15 17:39:40 +0800

    feat: add copy in the menu

--- apps/community/src/components/ConnectedWalletButton/index.tsx
+++ apps/community/src/components/ConnectedWalletButton/index.tsx
@@ -14,12 +14,14 @@ import {
   Icon,
   VStack,
   usePopoverContext,
+  Center,
 } from '@chakra-ui/react'
 import {
   useTrackClick,
   TrackEvent,
   TrackKey,
   CommunityClickCommunityPersonalcenterItem,
+  useCopyWithStatus,
 } from 'hooks'
 import { Avatar, AvatarProps } from 'ui'
 import { useTranslation } from 'react-i18next'
@@ -27,13 +29,16 @@ import { ReactComponent as CoAuthosSvg } from 'assets/svg/co-authors.svg'
 import { ReactComponent as InformationSvg } from 'assets/svg/information.svg'
 import { ReactComponent as DisconnectSvg } from 'assets/svg/disconnect.svg'
 import { ReactComponent as ChangeWalletSvg } from 'assets/svg/change_wallet.svg'
+import { ReactComponent as CopySvg } from 'assets/svg/copy.svg'
 import { Link } from 'react-router-dom'
+import { CheckIcon } from '@chakra-ui/icons'
 import { useLogout } from '../../hooks/useLogin'
 import { useOpenConnectWalletDialog } from '../../hooks/useConnectWalletDialog'
 import { RoutePath } from '../../route/path'
 import { useUserInfo } from '../../hooks/useUserInfo'
 import { formatUserName } from '../../utils/string'
 import { APP_URL } from '../../constants/env/url'
+import { useToast } from '../../hooks/useToast'
 
 export interface ConnectedWalletButtonProps extends ButtonProps {}
 
@@ -42,8 +47,10 @@ export const ConnectedWalletButtonMenu: React.FC<ButtonProps> = ({
 }) => {
   const context = usePopoverContext()
   const userInfo = useUserInfo()
+  const { onCopy, isCopied } = useCopyWithStatus()
   const { t } = useTranslation('components')
   const logout = useLogout()
+  const toast = useToast()
   const onOpenConnectWalletDialog = useOpenConnectWalletDialog()
 
   const trackClickCommunityPersonalCenterItem = useTrackClick(
@@ -76,22 +83,46 @@ export const ConnectedWalletButtonMenu: React.FC<ButtonProps> = ({
         <Icon w="20px" h="20px" as={InformationSvg} mr="8px" />
         {t('connect_wallet_button.information')}
       </Button>
-      <Button
-        as="a"
-        href={subPageUrl}
-        target="_blank"
-        variant="unstyled"
-        onClick={() => {
-          trackClickCommunityPersonalCenterItem({
-            [TrackKey.CommunityClickCommunityPersonalcenterItem]:
-              CommunityClickCommunityPersonalcenterItem.SubscriptionPage,
-          })
-        }}
-        {...props}
-      >
-        <Icon w="20px" h="20px" as={CoAuthosSvg} mr="8px" />
-        {t('connect_wallet_button.subscription')}
-      </Button>
+      <Center>
+        <Button
+          as="a"
+          href={subPageUrl}
+          target="_blank"
+          variant="unstyled"
+          onClick={() => {
+            trackClickCommunityPersonalCenterItem({
+              [TrackKey.CommunityClickCommunityPersonalcenterItem]:
+                CommunityClickCommunityPersonalcenterItem.SubscriptionPage,
+            })
+          }}
+          {...props}
+        >
+          <Icon w="20px" h="20px" as={CoAuthosSvg} mr="8px" />
+          {t('connect_wallet_button.subscription')}
+        </Button>
+        <Button
+          variant="unstyled"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          h="full"
+          w="40px"
+          onClick={() => {
+            onCopy(subPageUrl)
+            toast('Copy Successfully', {
+              status: 'success',
+              alertProps: { colorScheme: 'green' },
+            })
+          }}
+          style={{ cursor: isCopied ? 'default' : undefined }}
+        >
+          {isCopied ? (
+            <CheckIcon w="16px" h="16px" />
+          ) : (
+            <Icon as={CopySvg} w="20px" h="20px" />
+          )}
+        </Button>
+      </Center>
       <Button
         variant="unstyled"
         {...props}
@@ -164,7 +195,7 @@ export const ConnectedWalletButton: React.FC<ConnectedWalletButtonProps> = ({
           </Text>
         </Button>
       </PopoverTrigger>
-      <PopoverContent w="204px" top="10px">
+      <PopoverContent w="240" top="10px">
         <PopoverArrow />
         <PopoverBody py="16px" px="8px">
           <ConnectedWalletButtonMenu {...listItemProps} />
