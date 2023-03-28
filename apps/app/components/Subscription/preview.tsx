fcd09b3a8097425e805d9dbd7be45b5382446684
shadow2kk <horong.eth@mail3.me>
2023-03-28 14:15:41 +0800

    fix: miss replace

--- apps/app/components/Subscription/preview.tsx
+++ apps/app/components/Subscription/preview.tsx
@@ -364,7 +364,7 @@ export const SubPreview: React.FC<{ isSingleMode: boolean }> = ({
             shareUrl={`${APP_URL}/p/${id}`}
             text={detail.subject}
             iconW="22px"
-            uuid={id}
+            articleId={id}
           />
         </Flex>
         <Box fontWeight={500} fontSize="12px" color="#6F6F6F" mt="4px">
@@ -499,7 +499,7 @@ export const SubPreview: React.FC<{ isSingleMode: boolean }> = ({
           shareUrl={`${APP_URL}/p/${id}`}
           text={detail.subject}
           iconW="22px"
-          uuid={id}
+          articleId={id}
         />
       </Center>
     </Wrap>
