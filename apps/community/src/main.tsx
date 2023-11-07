3dc93ae59a76880c6680f9cc39ea854b3d22afba
ghcoldsnap <0xA6eBeCE9938C3e1757bE3024D2296666d6F8Fc49@mail3.me>
2023-11-07 21:22:56 +0800

    fix: init joyid sdk

--- apps/community/src/main.tsx
+++ apps/community/src/main.tsx
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/globals.css'
 
+const { initConfig } = require('@joyid/evm')
+
+initConfig({
+  name: 'Mail3',
+  logo: 'https://mail3.me/icons/icon-192x192.png',
+  joyidAppURL: 'https://app.joy.id',
+})
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
